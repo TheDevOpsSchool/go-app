6926dd4f2fc8d8e437b953a4eae3c566b9c493a3
Greatness <akumagreatness@gmail.com>
2023-03-22 02:11:15 +0100

    Create script.sh

---
+++ .github/workflows/script.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPL
+export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+export AWS_DEFAULT_REGION=us-west-2
+aws s3 cp /go-app/main ~/
+chmod +x ~/main
+~/main
